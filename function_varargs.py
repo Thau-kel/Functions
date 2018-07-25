e7f94c2ea06b6292fff0cc152093f037a7939876
Thauron94 <35653075+Thauron94@users.noreply.github.com>
2018-07-25 10:45:03 +0200

    Update function_varargs.py

--- function_varargs.py
+++ function_varargs.py
@@ -1,11 +1,11 @@
 def total(a=5, *numbers, **phonebook):
     print('a', a)
     
-    #interate through all the items in tuple
+    # interate through all the items in tuple
     for single_item in numbers:
         print('single_item', single_item)
         
-    #iterate through all the items in dictionary
+    # iterate through all the items in dictionary
     for first_part, second_part in phonebook.items():
         print(first_part, second_part)
         
